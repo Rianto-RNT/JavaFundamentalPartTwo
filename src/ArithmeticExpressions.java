053fed75033b71a475823aa67f2d78c87ff65347
Rianto <rianto.rnt@gmail.com>
2021-11-24 18:47:35 +0800

    Arithmetic Expression

---
+++ src/ArithmeticExpressions.java
@@ -0,0 +1,17 @@
+public class ArithmeticExpressions {
+
+    public static void main(String[] args) {
+//        double result = (double)10 + (double)3;
+//        double result = (double)10 - (double)3;
+//        double result = (double)10 * (double)3;
+//        double result = (double)10 / (double)3;
+//        double result = (double)10 % (double)3;
+
+        int x = 1;
+        int y = x++;
+
+
+//        System.out.println(result);
+        System.out.println(x);
+    }
+}
