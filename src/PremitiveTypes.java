d06a713060874d79f643e57499fdcad2f4a7397b
Rianto <rianto.rnt@gmail.com>
2021-11-24 14:26:46 +0800

    premitive type and reference type

--- src/PremitiveTypes.java
+++ src/PremitiveTypes.java
@@ -1,3 +1,17 @@
 public class PremitiveTypes {
-    
+    public static void main(String[] args) {
+
+        byte age = 28;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
+        System.out.println(age);
+        System.out.println(viewsCount);
+        System.out.println(price);
+        System.out.println(letter);
+        System.out.println(isEligible);
+
+    }
 }
---
+++ src/ReferenceType.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class ReferenceType {
+    public static void main(String[] args) {
+        byte age = 28;
+        Date now = new Date();
+        now.getTime();
+
+        System.out.println(now);
+    }
+}
